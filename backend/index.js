81c5d5d815489b89f06be9e00452ece3d7ae04f5
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-10-16 08:06:42 +0530

    change the codebase into nextjs

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const rootRouter = require("./routes/index");
-const app = express();
-
-app.use(express.json());
-app.use("/api/v1", rootRouter);
-
-module.exports = app;
